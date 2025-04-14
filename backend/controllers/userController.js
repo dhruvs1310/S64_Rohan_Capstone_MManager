117d03e92ec8fa38a2a92fe59facb5e3d7bbe018
Rohan Singh Jaglan <rohan.singh@kalvium.community>
2025-04-14 10:39:07 +0530

    Implemented Authentication[Login/Username]

---
+++ backend/controllers/userController.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+import { v2 as cloudinary } from 'cloudinary'
+// API to login user
+const loginUser = async (req, res) => {
+  try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "User does not exist" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: "Invalid credentials" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export {
+    loginUser,
+    registerUser,  
+}
